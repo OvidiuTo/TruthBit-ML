57171ae25e09a79f439ccf33639fbbb7f1e66e25
OvidiuTo <ovidiutoma876@gmail.com>
2024-05-26 11:46:45 +0300

    Add lib/api.dart to .gitignore

--- lib/api.dart
+++
@@ -1,8 +0,0 @@
-const String apiKey = "aa7cb045452440e0a5a721e20ee65d21";
-
-String country = "us";
-
-String baseUrl = 'https://newsapi.org/v2/top-headlines?country=';
-String apiKeyString = '&apiKey=$apiKey';
-
-    // "https://newsapi.org/v2/top-headlines?country=$country&apiKey=$apiKey";
